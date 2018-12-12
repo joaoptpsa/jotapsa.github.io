31b4ffff0b5631656a2b94e7699ffcec563b22ea
João Sá <up201506252@fe.up.pt>
2018-12-12 13:47:26 +0000

    :construction: :hankey:

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+
+import MainContainer from './mainContainer';
 
 class App extends React.Component {
   render() {
-    return null;
+    return <MainContainer />;
   }
 }
 
---
+++ src/app/mainContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Header } from 'semantic-ui-react';
+
+const MainContainer = () => (
+  <Container text>
+    <Header as="h1">Lorem Ipson</Header>
+
+
+
+
+
+
+    ASD
+</Container>
+);
+
+export default MainContainer;
